df118681869b903ba8a3e7bd6c693c956b82e5da
Rishika Arora <rishikarora2006@gmail.com>
2025-11-02 15:25:20 +0530

    exp 7 go back N

--- 7-go-back-N/gobackN.java
+++ 7-go-back-N/gobackN.java
@@ -1,5 +1,42 @@
-
+import java.util.*;
 
 public class gobackN {
-    
+    public static void main(String[] args) {
+        int base=0;
+        int window=0;
+        int frames=0;
+        int nextseqnum=0;
+        Random r= new Random();
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter window size:");
+        window=sc.nextInt();
+        System.out.println("enter total number of frames:");
+        frames=sc.nextInt();
+
+        while(base<frames){// until all acks keep sending , main loop hai ye
+            while(nextseqnum<frames && nextseqnum<base+window){
+                System.out.println("frame sent: "+ nextseqnum);
+                nextseqnum++;// send window size frames at once, then wait for acks
+            }
+            try {
+                Thread.sleep(1000);
+                
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+
+            Boolean ifsent= r.nextBoolean();
+
+            if(ifsent){
+                System.out.println("ACK received from frame:" + base);
+                base++;
+            }
+            else{
+                System.out.println("No ACK Rreceived from frame" + base);
+                nextseqnum=base;
+            }
+
+        }
+    }
 }
